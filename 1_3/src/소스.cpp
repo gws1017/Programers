0f4dd822daf09dfe1dd3d81be4a1d525619e76ef
gws1017 <65538479+gws1017@users.noreply.github.com>
2022-06-11 20:41:54 +0900

    [Level 1] 숫자 문자열과 영단어(C)#2
    - 예외 처리가 문제가 아니였다.
    - '0' 문자가 포함되지않음 범위가 '1' ~ '9' 였다.
    - 문자열 검사를 할때 반복문을 0~9개의 문자형 숫자를 검사하므로 10번 반복해야하는데 0부터 8까지만 검사하고 있었다.(int i -0; i<9; ++i)
    - 알아두면 좋은함수 strncmp() 두 문자열을 마지막 인자 값 만큼 길이만 검사하는 함수

--- 1_3/src/소스.cpp
+++ 1_3/src/소스.cpp
@@ -1,7 +1,3 @@
-//#include <string>
-//#include <vector>
-//
-//using namespace std;
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -9,12 +5,12 @@
 
 int checkword(const char* word)
 {
-    char str_num[10][10] = 
+    char *str_num[] = 
     { "zero","one","two","three" ,"four" ,
         "five" ,"six" ,"seven" ,"eight" ,"nine" };
-    for (int i = 0; i < 9; ++i)
+    for (int i = 0; i < 10; ++i)
     {
-        if (strcmp(word, str_num[i]) == 0)
+        if (strncmp(word, str_num[i],strlen(str_num[i])) == 0)
             return i;
     }
 
@@ -23,58 +19,48 @@ int checkword(const char* word)
 
 int solution(const char* s) {
     int answer = 0;
-    
-    int i = 0;
+
+    char ch = 0;
     int j = 0;
     int count = 0;
-    int words[50] = { 0 };
     char word[10] = "\0";
 
     int ret_value = -1;
-
-    while (s[i] != '\0')
+    int result = 0;
+    while (ch = *s)
     {
-        if (s[i] != ' ' || s[i] != '\0')
+        if ('0' <= ch && ch <= '9')
         {
-            if ('1' <= s[i] && s[i] <= '9')
-            {
-                words[count++] = s[i] - '0';
-            }
-            else
-            {
-                word[j++] = s[i];
-            }
-            ret_value = checkword(word);
-            if (-1 != ret_value)
-            {
-                words[count++] = ret_value;
-                j = 0;
-                memset(word, 0, 10 * sizeof(char));
-            }
+            result *= 10; //shift ¿¬»ê
+            result += (ch - '0');
         }
-        
-        i++;
-    }
-    for (int k = 0, m = count; m > 0; k++, m--)
-    {
-        int pow_value = 1;
-        for (int l = 0; l < m -1; l++)
+        else if( 'a' <= ch && 'z' >= ch)
         {
-            pow_value *= 10;
+             word[j++] = ch;
         }
-        answer += words[k] * pow_value;
+        ret_value = checkword(word);
+        if (-1 != ret_value)
+        {
+            result *= 10;
+            result += ret_value;
+            j = 0;
+            memset(word, 0, 10 * sizeof(char));
+        }
 
+        s++;
     }
+
+    answer = result;
     return answer;
 }
 
 int main()
 {
     char str[] = "one4seveneight";
-    char str2[] = "foursix";
+    char str2[] = "four00zero";
     char str3[] = "1zerotwozero3";
-    char str4[] = "2three45sixseven";
-    char str5[] = "123";
+    char str4[] = "2three45sixsevennine";
+    char str5[] = "sevensevenseven3sevensevensevenseven";
 
     printf("%d\n", solution(str2));
     printf("%d\n", solution(str4));
